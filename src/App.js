9d2f475624dc420441b4384ab4d06ad1bfb1910a
GugaValenca <gustavo_valenca@hotmail.com>
2025-08-05 18:02:56 -0500

    feat: initial movie watchlist app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header/Header";
+import Stats from "./components/Stats/Stats";
+import MovieForm from "./components/MovieForm/MovieForm";
+import FilterButtons from "./components/FilterButtons/FilterButtons";
+import MovieList from "./components/MovieList/MovieList";
+import Footer from "./components/Footer/Footer";
+import { useMovies } from "./hooks/useMovies";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const {
+    movies,
+    addMovie,
+    toggleWatched,
+    deleteMovie,
+    totalMovies,
+    watchedMovies,
+    unwatchedMovies,
+  } = useMovies();
+
+  const [currentFilter, setCurrentFilter] = useState("all");
+
+  const getFilteredMovies = () => {
+    switch (currentFilter) {
+      case "watched":
+        return movies.filter((movie) => movie.isWatched);
+      case "unwatched":
+        return movies.filter((movie) => !movie.isWatched);
+      default:
+        return movies;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Stats
+        total={totalMovies}
+        watched={watchedMovies}
+        unwatched={unwatchedMovies}
+      />
+      <MovieForm onAddMovie={addMovie} />
+      <FilterButtons
+        currentFilter={currentFilter}
+        onFilterChange={setCurrentFilter}
+        counts={{
+          total: totalMovies,
+          watched: watchedMovies,
+          unwatched: unwatchedMovies,
+        }}
+      />
+      <MovieList
+        movies={getFilteredMovies()}
+        onToggleWatched={toggleWatched}
+        onDeleteMovie={deleteMovie}
+        currentFilter={currentFilter}
+        totalMovies={totalMovies}
+      />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
